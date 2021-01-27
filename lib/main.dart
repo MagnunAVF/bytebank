490252e65420e15c980d53daa2c35cf25a118310
Magnun A V F <magnunavf@gmail.com>
2021-01-27 03:04:18 -0300

    Extract TransferItem and TransfersList to new StatelessWidgets

--- lib/main.dart
+++ lib/main.dart
@@ -2,24 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
       home: Scaffold(
-        body: Column(
-          children: [
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text('100.0'),
-                subtitle: Text('1000'),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text('100.0'),
-                subtitle: Text('1000'),
-              ),
-            ),
-          ],
-        ),
+        body: TransfersList(),
         appBar: AppBar(
           title: Text('Transfers'),
         ),
@@ -28,3 +11,40 @@ void main() => runApp(MaterialApp(
         ),
       ),
     ));
+
+class TransfersList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TransferItem(Transfer(100.0, 1000)),
+        TransferItem(Transfer(200.0, 1001)),
+        TransferItem(Transfer(300.0, 2000)),
+      ],
+    );
+  }
+}
+
+class TransferItem extends StatelessWidget {
+  final Transfer _transfer;
+
+  TransferItem(this._transfer);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transfer.value.toString()),
+        subtitle: Text(_transfer.accountNumber.toString()),
+      ),
+    );
+  }
+}
+
+class Transfer {
+  final double value;
+  final int accountNumber;
+
+  Transfer(this.value, this.accountNumber);
+}
