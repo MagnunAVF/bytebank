198607f485fd852c03f3382a37eb0156785153fc
Magnun A V F <magnunavf@gmail.com>
2021-02-07 17:05:15 -0300

    Create add and substract methods in balance

--- lib/screens/dashboards/dashboard.dart
+++ lib/screens/dashboards/dashboard.dart
@@ -1,4 +1,6 @@
+import 'package:bytebank/models/balance.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'balance.dart';
 
 const _appBarTitle = 'Bytebank';
@@ -8,9 +10,23 @@ class Dashboard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(_appBarTitle)),
-      body: Align(
-        alignment: Alignment.topCenter,
-        child: BalanceCard(),
+      body: ListView(
+        children: <Widget>[
+          Align(
+            alignment: Alignment.topCenter,
+            child: BalanceCard(),
+          ),
+          Consumer<Balance>(
+            builder: (context, balance, child) {
+              return RaisedButton(
+                child: Text('Add'),
+                onPressed: () {
+                  balance.add(10);
+                },
+              );
+            },
+          ),
+        ],
       ),
     );
   }
